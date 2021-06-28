a2d1a64e57809dbee9db245ea753870b1f7f18ea
unknown <any.nguyen96@gmail.com>
2021-06-28 19:52:15 +0700

    Creat a new VNDtoUSD.py

---
+++ VNDtoUSD.py
@@ -0,0 +1,18 @@
+def vToUConverter():
+    vMoney = input("Please enter Viet nam dong number:")
+    if vMoney:
+        vMoney = int(vMoney)
+        uMoney = round (vMoney/23000 , 2)
+
+
+    print(f"{vMoney}VND VietNamdong is convert to {uMoney}USD")
+
+
+
+
+def main():
+    vToUConverter()
+
+if __name__ == "__main__":
+        main()
+
