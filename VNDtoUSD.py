fbcecb3766c669ac86accb9f84b7198c357b7893
Any Kim <86566709+anyiscoding@users.noreply.github.com>
2021-06-28 20:21:38 +0700

    Update VNDtoUSD.py

--- VNDtoUSD.py
+++ VNDtoUSD.py
@@ -3,16 +3,9 @@ def vToUConverter():
     if vMoney:
         vMoney = int(vMoney)
         uMoney = round (vMoney/23000 , 2)
-
-
     print(f"{vMoney}VND VietNamdong is convert to {uMoney}USD")
-
-
-
-
 def main():
     vToUConverter()
-
 if __name__ == "__main__":
         main()
 
